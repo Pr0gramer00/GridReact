af922eb808c7188e5360c615bc78396e9899daa9
Dusan Sarac <duka.sarac@gmail.com>
2022-11-17 18:33:48 +0100

    Dodavanje AddEventListenera na klik

--- grid-react/src/Components/Header/Header.jsx
+++ grid-react/src/Components/Header/Header.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import './Header.css'
 
+const Kliknuto = ()=> {
+  const changetext1 = document.getElementById ('klik1').innerHTML = 'NAVIGACIJA';
+  const changetext2 = document.getElementById ('klik2').innerHTML = 'SIDE MENI';
+  const changetext3 = document.getElementById ('klik3').innerHTML = 'KUTIJA 1';
+  const changetext4 = document.getElementById ('klik4').innerHTML = 'NEKE INFORMACIJE';
+}
 const Header = () => {
   return (
     <div className="container">
-      <nav className='navbar'>Navbar</nav>
-      <div className="sidebar">Sidebar</div>
-      <div className="box">Box1</div>
+      <button onClick={Kliknuto}>Izmijeniti</button>
+      <nav className='navbar' id='klik1'>Navbar</nav>
       
-      <div className="footer">Footer</div>
+      <div className="sidebar" id='klik2'>Sidebar</div>
+      <div className="box" id='klik3'>Box1</div>
+      
+      <div className="footer" id='klik4'>Footer</div>
 
 
 
